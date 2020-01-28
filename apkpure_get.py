03517616fd5c196542caaa65343d8f5309d729d6
Zack Horvath <zack.horvath@branch.io>
2020-01-27 20:32:04 -0800

    Added new argument and versioning capability

--- apkpure_get.py
+++ apkpure_get.py
@@ -4,25 +4,56 @@ from __future__ import print_function
 from bs4 import BeautifulSoup
 import progressbar
 import requests
-import sys
+import argparse
+import re
+
 ### XXX: hack to skip some stupid beautifulsoup warnings that I'll fix when refactoring
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
+###
 
-def get_apk(app_name): # XXX: this function needs refactoring/cleaning.
-    print("{+} getting download link for %s" %(app_name))
+# Set Arguments
+about = "This script downloads versioned APKs from APKPure."
+parser = argparse.ArgumentParser(description = about)
+parser.add_argument("-a", "--app", required=True, type=str)
+parser.add_argument("-v", "--version", default=None, type=str)
+args = parser.parse_args()
+app_name = args.app
+app_version = args.version
+
+# Download Latest (Default)
+def get_apk(app):
     site = "https://apkpure.com"
-    url = "https://apkpure.com/search?q=%s" %(app_name)
+    url = "https://apkpure.com/search?q=%s" % app_name 
     html = requests.get(url)
-    parse = BeautifulSoup(html.text)
+    parse = BeautifulSoup(html.text, "html.parser")
     for i in parse.find("p"):
         a_url = i["href"]
         app_url = site + a_url + "/download?from=details"
         html2 = requests.get(app_url)
-        parse2 = BeautifulSoup(html2.text)
+        parse2 = BeautifulSoup(html2.text, "html.parser")
         for link in parse2.find_all("a",id="download_link"):
             download_link = link["href"]
-        download_apk(app_name, download_link)
+        download_apk(app, download_link)
+
+# Download Versioned APK
+def get_apk_ver(app):
+    site = "https://apkpure.com"
+    url = "https://apkpure.com/search?q=%s" % app_name 
+    html = requests.get(url)
+    parse = BeautifulSoup(html.text, 'html.parser')
+    regex = re.compile((format(app_version)))
+    for i in parse.find("p"):
+        a_url = i["href"]
+        app_landing_url = site + a_url
+        html2 = requests.get(app_landing_url)
+        parse2 = BeautifulSoup(html2.text, 'html.parser')
+        download_landing_url = site + parse2.find("a", title=regex)["href"]
+        html3 = requests.get(download_landing_url)
+        parse3 = BeautifulSoup(html3.text, 'html.parser')
+        for link in parse3.find_all("a",id="download_link"):
+            download_link = link["href"]
+        download_apk(app, download_link)
 
 def make_progress_bar():
     return progressbar.ProgressBar(
@@ -38,9 +69,13 @@ def make_progress_bar():
             ') ',
         ])
 
-def download_apk(app_name, download_link):
-    print("{+} downloading %s" %(app_name))
-    output_file = "output/" + app_name + ".apk"
+def download_apk(app, download_link):
+    if args.version:
+        print ("Downloading " + app_name + " v" + app_version)
+        output_file = "output/" + app_name + "-" + app_version + ".apk"
+    elif args.app:
+        print("Downloading " + app_name + "latest")
+        output_file = "output/" + app_name + ".apk"
     r = requests.get(url=download_link, stream=True)
     with open(output_file, 'wb') as f:
         total_length = int(r.headers.get('content-length'))
@@ -54,12 +89,9 @@ def download_apk(app_name, download_link):
                 f.write(chunk)
                 f.flush()
         bar.finish()
-    print("{+} done. file saved to %s" %(output_file))
+    print("File saved to %s" %(output_file))
 
-def main(args):
-    if len(args) != 2:
-        sys.exit("use: %s com.blah.blah" %(args[0]))
-    get_apk(args[1])
-
-if __name__ == "__main__":
-    main(args=sys.argv)
+if args.version:
+    get_apk_ver(app_name)
+elif args.app:
+    get_apk(app_name)
